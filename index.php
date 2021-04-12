93412eda61699dc1111b64a3c6480f1e69d68a5f
TrungNguyen12082001 <77619154+TrungNguyen12082001@users.noreply.github.com>
2021-04-12 10:11:55 +0700

    change link

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <link rel="stylesheet" href="jquerymobile/jquery.mobile-1.4.5.min.css" />
-    <script src="jquerymobile/jquery-1.11.1.min.js"></script>
-    <script src="jquerymobile/jquery.mobile-1.4.5.min.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
     <!--
 	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css" />
 	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
