4297f0e2b6abc135f8e4596f1675d100c3a2fe07
TrungNguyen12082001 <77619154+TrungNguyen12082001@users.noreply.github.com>
2021-04-19 08:59:50 +0700

    ATN

--- ATN/product_create.php
+++ ATN/product_create.php
@@ -3,7 +3,22 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+echo "This file is ";
+?>
+
+<?php
+    $id = $_REQUEST["id"];
+    $name = $_REQUEST["name"];
+    $cat = $_REQUEST["cat"];
+    $date = $_REQUEST["date"];
+    $price = $_REQUEST["price"];
+    $desc = $_REQUEST["desc"];
+    echo $$id;
+    echo $$name;
+    echo $cat;
+    echo $date;
+    echo $price;
+    echo $desc;
 ?>
 
 </body>
---
+++ demo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Create a product</h1>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
